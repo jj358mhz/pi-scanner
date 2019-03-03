4c2a0ccae1e772e246403cbcff693084da0e70c7
Jeff Johnston <jj358mhz@gmail.com>
2019-03-03 09:23:29 -0800

    Added comments & renamed file

--- alerts_email.py
+++ alerts_email.py
@@ -2,9 +2,9 @@
 
 ##################################################################
 
-# alert_email.py by Jeff Johnston <jj358mhz@gmail.com>
+# alerts_email.py by Jeff Johnston <jj358mhz@gmail.com>
 # Alerting script for Scanner Listeners
-# THIS FILE: /usr/local/bin/alert_email.py
+# THIS FILE: /usr/local/bin/alerts_email.py
 # No Warranty is implied, promised or permitted.
 
 ##################################################################
@@ -14,13 +14,13 @@ import json
 import os
 import urllib2
 
-feedId = ''
-username = ''
-password = ''
-email = ''
+feedId = '' # ENTER YOUR BROADCASTIFY FEED ID HERE
+username = '' # ENTER YOUR BROADCASTIFY USERNAME HERE
+password = '' # ENTER YOUR BROADCASTIFY PASSWORD HERE
+email = '' # ENTER YOUR ALERT DESTINATION EMAIL HERE
 
-alertThreshold = 5
-alertSubject = "[YOUR CHANNEL NAME HERE] Broadcastify Alert"
+alertThreshold = 5 # ENTER YOUR DESIRED ALERT LISTENER THRESHOLD HERE
+alertSubject = "[YOUR CHANNEL NAME HERE] Broadcastify Alert" # ENTER YOUR BROADCASTIFY CHANNEL NAME HERE
 alertBody = "Broadcastify listener threshold exceeded " + str(alertThreshold) + " listeners.  "
 alertBody += "Listen to the feed here http://www.broadcastify.com/listen/feed/" + feedId + "  "
 alertBody += "Manage the feed here http://www.broadcastify.com/manage/feed/" + feedId + "  "
