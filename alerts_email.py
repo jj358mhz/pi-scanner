c49a0490c5d6fb84ca4651803817d5d8dffb5bb4
Jeff Johnston <jj358mhz@gmail.com>
2019-03-03 10:32:15 -0800

    Reformatting

--- alerts_email.py
+++ alerts_email.py
@@ -3,7 +3,7 @@
 ##################################################################
 
 # alerts_email.py by Jeff Johnston <jj358mhz@gmail.com>
-# Alerting script for Scanner Listeners
+# Alerting script (Email) for Scanner Listeners
 # THIS FILE: /usr/local/bin/alerts_email.py
 # No Warranty is implied, promised or permitted.
 
@@ -14,13 +14,15 @@ import json
 import os
 import urllib2
 
-feedId = '' # ENTER YOUR BROADCASTIFY FEED ID HERE
-username = '' # ENTER YOUR BROADCASTIFY USERNAME HERE
-password = '' # ENTER YOUR BROADCASTIFY PASSWORD HERE
-email = '' # ENTER YOUR ALERT DESTINATION EMAIL HERE
+feedId = ''  # ENTER YOUR BROADCASTIFY FEED ID HERE
+username = ''  # ENTER YOUR BROADCASTIFY USERNAME HERE
+password = ''  # ENTER YOUR BROADCASTIFY PASSWORD HERE
+email = ''  # ENTER YOUR ALERT DESTINATION EMAIL HERE
 
-alertThreshold = 5 # ENTER YOUR DESIRED ALERT LISTENER THRESHOLD HERE
-alertSubject = "[YOUR CHANNEL NAME HERE] Broadcastify Alert" # ENTER YOUR BROADCASTIFY CHANNEL NAME HERE
+# This threshold amount is the number of listeners that need to be exceeded before email alerts are sent out
+alertThreshold = 20  # ENTER YOUR DESIRED ALERT LISTENER THRESHOLD HERE
+
+alertSubject = "[YOUR CHANNEL NAME HERE] Broadcastify Alert"  # ENTER YOUR BROADCASTIFY FEED NAME HERE
 alertBody = "Broadcastify listener threshold exceeded " + str(alertThreshold) + " listeners.  "
 alertBody += "Listen to the feed here http://www.broadcastify.com/listen/feed/" + feedId + "  "
 alertBody += "Manage the feed here http://www.broadcastify.com/manage/feed/" + feedId + "  "
