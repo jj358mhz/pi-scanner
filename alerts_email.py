6769ddb98b033e9fcfa4c35a6396135fae43e30f
Jeff Johnston <jj358mhz@gmail.com>
2019-03-04 11:47:51 -0800

    Updated tuple index

--- alerts_email.py
+++ alerts_email.py
@@ -22,16 +22,18 @@ email = ''  # ENTER YOUR ALERT DESTINATION EMAIL HERE
 # This threshold amount is the number of listeners that need to be exceeded before email alerts are sent out
 alertThreshold = 20  # ENTER YOUR DESIRED ALERT LISTENER THRESHOLD HERE
 
-alertSubject = "[YOUR CHANNEL NAME HERE] Broadcastify Alert"  # ENTER YOUR BROADCASTIFY FEED NAME HERE
-alertBody = "Broadcastify listener threshold exceeded " + str(alertThreshold) + " listeners.  "
-alertBody += "Listen to the feed here http://www.broadcastify.com/listen/feed/" + feedID + "  "
-alertBody += "Manage the feed here http://www.broadcastify.com/manage/feed/" + feedID + "  "
-
 url = "https://api.broadcastify.com/owner/?a=feed&feedId=" + feedID + "&type=json&u=" + username + "&p=" + password
 response = urllib2.urlopen(url)
 data = json.load(response)
+descr = data['Feed'][0]['descr']
 listeners = data['Feed'][0]['listeners']
-alertBody += "The current number of listeners is " + str(listeners) + "  "
+
+
+alertSubject = "[{}] Broadcastify Alert".format(descr)
+alertBody = "Broadcastify listener threshold {} exceeded ".format(alertThreshold) + " listeners.  "
+alertBody += "Listen to the feed here http://www.broadcastify.com/listen/feed/" + feedID + "  "
+alertBody += "Manage the feed here http://www.broadcastify.com/manage/feed/" + feedID + "  "
+alertBody += "The current number of listeners is {}".format(listeners) +  "  "
 
 if listeners > alertThreshold:
     cmd = 'echo ' + alertBody + ' | mail -s "' + alertSubject + '" ' + email
